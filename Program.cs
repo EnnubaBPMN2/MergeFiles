3c38231c28da457e858b5f3d463bba847386400d
ENNUBA <34802457+EnnubaBPMN2@users.noreply.github.com>
2019-11-12 18:31:59 -0500

    Initial Solution

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Spire.Pdf;
+
+namespace MergeFiles
+{
+    class MergeFiles
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: MergeFiles  Filename1  Filename2");
+                return;
+            }
+
+            var filesStreams = new List<Stream>
+            {
+                File.OpenRead(args[0]),
+                File.OpenRead(args[1])
+            };
+
+            var outputFile = args[2];
+            
+            var result = PdfDocument.MergeFiles(filesStreams.ToArray());
+
+            result.Save(outputFile);
+        }
+    }
+}
